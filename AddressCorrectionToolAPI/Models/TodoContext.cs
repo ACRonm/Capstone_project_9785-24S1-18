606074aa56ef9ebb23c1734971384c31e591cf8b
Aiden <u3200426@uni.canberra.edu.au>
2024-02-25 16:20:05 +1100

    Initialise API

---
+++ AddressCorrectionToolAPI/Models/TodoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace AddressCorrectionToolAPI.Models
+{
+    public class AddressContext : DbContext
+    {
+        public AddressContext(DbContextOptions<AddressContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Address> Address { get; set; }
+    }
+}
